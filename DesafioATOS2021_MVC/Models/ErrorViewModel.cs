f74871c68adcb41f207b9eec928aba1483119e2a
Leonardo Rocha <leonardo.rocha@atos.net>
2021-11-29 17:49:02 -0400

    Add project files.

---
+++ DesafioATOS2021_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DesafioATOS2021_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
